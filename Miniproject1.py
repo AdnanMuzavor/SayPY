aae5cd879f8a3a9e71c7b3294b3603ba7f9f590d
Adnan Muzavor <www.adnan9168389872@gmail.com>
2021-12-28 16:31:50 +0530

    Driving menu, add book feature

--- Miniproject1.py
+++ Miniproject1.py
@@ -12,12 +12,34 @@ class Library:
         for i in range(int(count1)):
             book = input(f"Enter book no {i+1}: ")
             author = input(f"Enter author no {i+1}: ")
-            self.books[author] = book
+            self.books[author] = [book, "No"]
         self.display()
 
+    def addtoexisting(self):
+        book = input(f"Enter book : ")
+        author = input(f"Enter author : ")
+        self.books[author] = [book, "No"]
+
     def display(self):
+        print("Author\tBook\tBorrowed")
         for key, item in self.books.items():
-            print(f"{item} written by {key} ")
+            print(f"{key}\t{item[0]}\t{item[1]} ")
 
 
-l1 = Library("Libos")
+while True:
+    n = input("Enter 1 make library\nEnter 2 to get all books displayed\nEnter 3 to borrow a book\nEnter 4 to return a book\nEnter 5 to add book in library\nEnter 6 to exit\nYour choice:  ")
+    print("\n")
+    if int(n) == 1:
+        l1 = Library("Libos")
+    elif int(n) == 2:
+        l1.display()
+    elif int(n) == 3:
+        print("Borrowing")
+    elif int(n) == 4:
+        print("Returning")
+    elif int(n) == 5:
+        l1.addtoexisting()
+    elif int(n) == 6:
+        break
+    else:
+        print(f"{n} is invalid input")
